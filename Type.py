98e606232f93e1d114e08cfaf3704647603a7179
AntonioLimaJ <122129418+AntonioLimaJ@users.noreply.github.com>
2023-02-19 11:16:37 -0300

    Studying the dynamic typing on python

---
+++ Type.py
@@ -0,0 +1,13 @@
+country = str("Brazil")
+age = 15
+money = 28.5
+bool = True
+
+print(
+    type(country),
+    type(age),
+    type(money),
+    type(bool) )
+# It's not completly necessary to put all the types on the variables
+# but if in the future you will use that variable to another thing
+# or if you will do a input, it's likeable that you define the type
